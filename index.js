6482a8f04e7f4d7bb98fb41ad2ce33ff7ca26325
youbaidjis <youba.etud@gmail.com>
2022-03-23 01:09:48 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const arrMessages=[];
+
+function writeCards(name, event){    
+    for(let i=0;i<name.length;i++){
+     arrMessages.push('Thank you, ' +name[i] +', for the wonderful '+event+ ' gift!');     
+    } 
+    return arrMessages;
+}
+
+function countDown(numberPro){
+     let i=numberPro;
+    while (i>=0){
+        console.log(i);
+        i--;
+    }
+    /*for(let i=numberPro;i>=0;i--){
+        console.log(i);
+    }*/
+}
+ 
